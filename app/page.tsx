bd0e478110ca745f0e21c20d2687b419c0a25293
hartman0791-maker <hartman0791@gmail.com>
2026-02-04 14:20:24 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,1 @@
-export default function DiscoverPage() {
-  return (
-    <main style={{ maxWidth: 520, margin: "40px auto", fontFamily: "system-ui" }}>
-      <h1>Discover</h1>
-      <p>If you can see this, the page is deploying correctly.</p>
-    </main>
-  );
-}
+
