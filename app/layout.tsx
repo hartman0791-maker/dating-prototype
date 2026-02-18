72a389bdd94caa3f68e1a8e5fdee5ada9fcc7a9d
hartman0791-maker <hartman0791@gmail.com>
2026-02-17 23:27:00 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import "./globals.css";
 import ThemeToggle from "../components/ThemeToggle";
+import PresenceProvider from "../components/PresenceProvider";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        {/* ✅ GLOBAL PRESENCE TRACKING */}
+        <PresenceProvider>
+          {children}
+        </PresenceProvider>
+
         <ThemeToggle />
       </body>
     </html>
