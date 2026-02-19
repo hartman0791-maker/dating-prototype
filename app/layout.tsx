999b45ff54063231b058be9488b52c7ef367e306
hartman0791-maker <hartman0791@gmail.com>
2026-02-19 11:10:09 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,16 @@
-// app/layout.tsx
 import "./globals.css";
 import ThemeToggle from "../components/ThemeToggle";
 import PresenceProvider from "../components/PresenceProvider";
-
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <PresenceProvider>{children}</PresenceProvider>
+        <PresenceProvider>
+          {children}
+        </PresenceProvider>
 
         <ThemeToggle />
-
-        {/* ✅ Toasts (global) */}
-        <Toaster
-          position="top-center"
-          richColors
-          closeButton
-          expand={false}
-          toastOptions={{
-            duration: 2200,
-            style: { borderRadius: 14, fontWeight: 800 },
-          }}
-        />
       </body>
     </html>
   );
